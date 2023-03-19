363e0b64cfda392e8b1d84ea3b89c7e2f8ff17e9
Houria-khouildat☺️ <128228965+Houria-Kouildat@users.noreply.github.com>
2023-03-19 15:06:06 -0700

    Update projet01.py

--- projet01.py
+++ projet01.py
@@ -52,7 +52,128 @@ def save():
         print("there is an error...")
 
 
+for i in range(0, len(H)) :
+        if ltr(H[i]) and ltr(H[i+1]):
+            s+=H[i]
+
+        elif let(H[i])     :
+
+            if( H[i+1]=='+'and  H[i+2]=='+' ) or ( H[i+1]=='-' and
+H[i+2]=='-') or (H[i+1]==' ') or (ope (H[i+1]) ) :
+                 s+=H[i]+'#'
+            elif  nem(H[i+1]) or(H[i+1]=='_') :
+                   s+=H[i]
+            else :
+                    s+=H[i]
+        elif nem (H[i]) :
+             if H[i+1]=='.' or let (H[i+1]):
+
+                 s+=H[i]
+             elif H[i+1]==' ':
+                 s+=H[i]+'#'
+             elif ope(H[i+1]):
+                   s+=H[i]+'#'
+             else:
+                    s+=H[i]
+        elif ope(H[i]):
+
+             if    (H[i]=='+'and H[i+1]=='+') or (H[i]=='-'and
+H[i+1]=='-') or ((H[i]=='<'or'>'or'='or'!'or ':') and (H[i+1]=='=' and
+(nem(H[i+2]) or ope (H[i+2]) or let(H[i+2])) )) or (H[i]=='.' and
+nem(H[i+1])) or(H[i]=='_' and nem(H[i+1])) or((H[i]=='+' or
+H[i]=='-')and H[i+1]=='.'and nem (H[i+2])) or(nem(H[i-1])and
+H[i]=='.'and nem(H[i+1])):
+                    s+= H[i]
+             elif ((H[i-2]=='+'and H[i-1]=='+'and H[i]=='+')or
+(H[i-2]=='-'and H[i-1]=='-'and H[i]=='-')) or((ope (H[i-1]) or let
+(H[i-1]))and (H[i]=='+'or H[i]=='-') and nem (H[i+1]) ) or     ((ope
+(H[i-1]) or let (H[i-1]))and (H[i]=='+'or H[i]=='-') and
+H[i+1]=='.'and nem (H[i+2])) :
+                   s+='#'+H[i]
+
+             elif (H[i]=='+'and H[i+1]!='+') or (H[i]=='-'and H[i+1]!='-'):
+                s+= H[i] +'#'
+
+
+             elif (H[i+1==' '])or (nem (H[i+1])or let (H[i+1])) :
+                s+=H[i]+'#'
+             else:
+               s+=H[i]
+    s+='#'
+           
+        
+        
 # ================================================================================================
+T=( [15,10,20, 7, 2, 5, 3, 1, 1, 3,-1],
+        [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+        [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+        [-1,-1,-1,-1,-1,-1, 4,-1,-1,-1,-1],
+        [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+        [-1,-1,-1,-1,-1,-1, 6,-1,-1,-1,-1],
+        [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+        [-1,-1,-1, 7,-1,-1,-1, 8,-1,-1,-1],
+        [-1,-1,-1, 9,-1,-1,-1,-1,-1,-1,-1],
+        [-1,-1,-1, 9,-1,-1,-1,-1,-1,-1,-1],
+        [-1,11,-1,12,-1,-1,-1,-1,-1,-1,-1],
+        [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+        [-1,-1,-1,12,-1,-1,-1,13,-1,-1,-1],
+        [-1,-1,-1,14,-1,-1,-1,-1,-1,-1,-1],
+        [-1,-1,-1,14,-1,-1,-1,-1,-1,-1,-1],
+        [16,-1,-1,17,-1,-1,-1,-1,-1,-1,-1],
+        [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,-1],
+        [-1,-1,-1,17,-1,-1,-1,18,-1,-1,-1],
+        [-1,-1,-1,19,-1,-1,-1,-1,-1,-1,-1],
+        [-1,-1,-1,19,-1,-1,-1,-1,-1,-1,-1],
+        [-1,-1,20,20,-1,-1,-1,-1,-1,-1,21],
+        [-1,-1,20,20,-1,-1,-1,-1,-1,-1,22],
+        [-1,-1,20,20,-1,-1,-1,-1,-1,-1,23],
+        [-1,-1,-1,-1,-1,-1,-1,-1,-1,-1,22],
+
+    )
+    ent2.delete("1.0","end")
+    i=0
+    z=''
+    k=''
+    ec=0
+
+    while i < (len(s)):
+        tc=s[i]
+        if type(tc) != 100:
+            z=z+s[i]
+            if ec != -1 and type(tc)!=-1:
+
+                print(T[ec][type(tc)])
+                ec=T[ec][type(tc)]
+        else:
+
+            if ec==1 :  k=k+" "+z+"  : est une separateur  ' ( )  [ ]
+{ } ; .' \n"
+            elif ec==2 :  k=k+" "+z+"  : est une operateur  *  / \n "
+            elif ec==3 :  k=k+" "+z+"  : est une operateur <  > = \n"
+            elif ec==4 :  k=k+" "+z+"  : est une operateur '== ' '<=' '>='\n "
+            elif ec==5 :  k=k+" "+z+"  : est  une seperateur  ! : \ \n"
+            elif ec==6 :  k=k+" "+z+"  : est une operateur  != :=  \n "
+            elif ec==7 :  k=k+" "+z+"  : est nombre entier \n"
+            elif ec==9 :  k=k+" "+z+"  : est nombre real \n"
+            elif ec==11 : k=k+" "+z+" : est pas negatif \n"
+            elif ec==12 : k=k+" "+z+" : est nombre entier signe  negatif \n "
+            elif ec==14 : k=k+" "+z+" : est nombre real signe negatif  \n "
+            elif ec==16 : k=k+" "+z+" : est pas positif \n"
+            elif ec==17 : k=k+" "+z+" : est nombre entier signe  positif \n "
+            elif ec==19 : k=k+" "+z+" : est nombre real signe positif  \n "
+            elif ec==20 : k=k+" "+z+" : est nombres ou letteres ou les
+deux avec '_' \n "
+            elif ec==15 : k=k+" "+z+" : is operateur positif \n"
+            elif ec==10 : k=k+" "+z+" : is operateur negatif \n"
+            else :        k=k+" "+z+" : is the erreur \n "
+            z= ''
+            ec =0
+        i=i+1
+
+
+
+    ent2.insert(END,k)
+   
 menuber = Menu(window)
 window.config(menu=menuber)
 # ===========================================================================================
